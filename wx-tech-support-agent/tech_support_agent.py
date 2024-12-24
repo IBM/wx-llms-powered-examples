b024d3a53b7172a361b56cbfc575f09f13bcd15e
Nguyen, Hung (Howie) Sy <howiesnguyen>
2024-12-24 10:16:39 +0000

    Refactoring

--- wx-tech-support-agent/tech_support_agent.py
+++ wx-tech-support-agent/tech_support_agent.py
@@ -55,7 +55,6 @@ class ChatMemory:
 
 class TechSupportAgent():
     def __init__(self):
-
         print("\nRequest the models from WatsonX...")
         self.llama_llm = WatsonxClient.request_llm(
                                     model_id="meta-llama/llama-3-1-70b-instruct",
@@ -70,10 +69,10 @@ class TechSupportAgent():
                                     stop_sequences=["<|end_of_text|>"])
 
         # Set up LangChain's ReAct framework
-        self._tools = [default_action, 
-             generate_a_clarifying_question, 
-             diagnosis_and_solution, 
-             escalate_to_human_support]
+        self._tools = [Tools.default_action, 
+             Tools.generate_a_clarifying_question, 
+             Tools.diagnosis_and_solution, 
+             Tools.escalate_to_human_support]
         
         self._prompt_template_react = PromptTemplate.from_template(prompts.PROMPT_TEMPLATE__REACT)
 
@@ -98,14 +97,15 @@ class TechSupportAgent():
 
     def query(self, user_input):
         try:
-            print(f"\nAgent: \033[90mPlease wait\033[0m", end=' ', flush=True)
+            self.chat_memory.add_user_message(user_input)
+
             for i in range(NUMBER_OF_RETRIES + 1):
                 print(f"\033[90m...\033[0m", end=' ', flush=True)
                 agent_response = None
                 
                 result = {}
                 try:
-                    result = agent_executor.invoke({"user_input": user_input, 
+                    result = self._agent_executor.invoke({"user_input": user_input, 
                                                     "chat_history": self.chat_memory.to_string()})
                     agent_response = result.get('output')
                 except Exception as e:
@@ -136,9 +136,6 @@ class TechSupportAgent():
             else:
                 agent_response = f"Sorry, something was wrong due to an error ({result.get('error')}). Please try entering the input again..."
 
-            print(" " * 100, end='\r')
-            # print(f"Agent: {agent_response}")
-            agent_streaming_print(agent_response)
         except Exception as error:
             agent_response = f"Program error: Something's wrong: {error}"
             print(f"\n{agent_response}")
@@ -149,8 +146,6 @@ class TechSupportAgent():
         self.chat_memory = ChatMemory()
 
 
-support_agent = TechSupportAgent()
-
 def agent_streaming_print(text: str, delay=0.005):
     print("Agent:", end=' ', flush=True)
     for char in text:
@@ -159,114 +154,101 @@ def agent_streaming_print(text: str, delay=0.005):
         time.sleep(delay)
     print(f"")  
 
-# Declare tool(s)
-@tool("default_action", return_direct=False)
-def default_action(input: str):
-    """This is the default action that the agent can take when three is no other action"""
-    
-    # a workaround to remove "\nObservation" that may be found at the end
-    input = input.removesuffix("\nObservation")
+class Tools():
+    @staticmethod
+    @tool("default_action", return_direct=False)
+    def default_action(input: str):
+        """This is the default action that the agent can take when three is no other action"""
+        
+        # a workaround to remove "\nObservation" that may be found at the end
+        input = input.removesuffix("\nObservation")
 
-    return ("Instruction for you the agent: Based on the last messages in the chat history, "
-            f"think about a relevant response in response to this user input '{input}'. "
-            "Do not take the next action or use another tool, instead provide the final answer/response.\n")
+        return ("Instruction for you the agent: Based on the last messages in the chat history, "
+                f"think about a relevant response in response to this user input '{input}'. "
+                "Do not take the next action or use another tool, instead provide the final answer/response.\n")
 
-@tool("generate_a_clarifying_question", return_direct=False)
-def generate_a_clarifying_question(input: str):
-    """Generating a clarifying question to gather relevant information about the issue.
-    This will allow the issue to be identified more clearly"""
+    @staticmethod
+    @tool("generate_a_clarifying_question", return_direct=False)
+    def generate_a_clarifying_question(input: str):
+        """Generating a clarifying question to gather relevant information about the issue.
+        This will allow the issue to be identified more clearly"""
 
-    prompt_template= PromptTemplate(input_variables=["chat_history"],
-                                    template=prompts.PROMPT_TEMPLATE__CLARIFYING_QUESTIONS)
-    prompt = prompt_template.format(chat_history=support_agent.chat_memory.to_string())
-    questions = support_agent.granite_llm.invoke(prompt)
-    questions = questions.strip().removesuffix("```").removeprefix("```")
+        global support_agent
 
-    response = ("The instruction for you, the agent: Refer to the JSON array below for clarifying questions that you can use to ask the user:"
-                f"\n{questions}\n" 
-                "Extract the questions from the JSON array and think about combining the chosen questions into a single response if needed, especially when they are intended to identify a specific product or the object of the problem/issue. "
-                )
+        prompt_template= PromptTemplate(input_variables=["chat_history"],
+                                        template=prompts.PROMPT_TEMPLATE__CLARIFYING_QUESTIONS)
+        prompt = prompt_template.format(chat_history=support_agent.chat_memory.to_string())
+        questions = support_agent.granite_llm.invoke(prompt)
+        questions = questions.strip().removesuffix("```").removeprefix("```")
 
-    # print("\033[90m(Debug: The generate_a_clarifying_question tool was invoked)\033[0m")
+        response = ("The instruction for you, the agent: Refer to the JSON array below for clarifying questions that you can use to ask the user:"
+                    f"\n{questions}\n" 
+                    "Extract the questions from the JSON array and think about combining the chosen questions into a single response if needed, especially when they are intended to identify a specific product or the object of the problem/issue. "
+                    )
 
-    return response
+        # print("\033[90m(Debug: The generate_a_clarifying_question tool was invoked)\033[0m")
 
-@tool("diagnosis_and_solution", return_direct=True)
-def diagnosis_and_solution(input: str):
-    """This is to perform the step Diagnosis and Solution Suggestion. 
-    Analyze gathered information to hypothesize the root cause of the issue. 
-    And then suggest a solution based on the diagnosis information. Provide step-by-step instructions for resolving the issue. 
-    Additionally, ask the user to confirm if the issue is resolved."""
-    prompt_template= PromptTemplate(input_variables=["chat_history"],
-                                    template=prompts.PROMPT_TEMPLATE__DIAGNOSIS_SOLUTION)
-    prompt = prompt_template.format(chat_history=support_agent.chat_memory.to_string())
-    response = support_agent.granite_llm.invoke(prompt)
+        return response
 
-    # print("\033[90m(Debug: The diagnosis_and_solution tool was invoked)\033[0m")
+    @staticmethod
+    @tool("diagnosis_and_solution", return_direct=True)
+    def diagnosis_and_solution(input: str):
+        """This is to perform the step Diagnosis and Solution Suggestion. 
+        Analyze gathered information to hypothesize the root cause of the issue. 
+        And then suggest a solution based on the diagnosis information. Provide step-by-step instructions for resolving the issue. 
+        Additionally, ask the user to confirm if the issue is resolved."""
 
-    return response
+        global support_agent
 
-@tool("escalate_to_human_support", return_direct=True)
-def escalate_to_human_support(input: str):
-    """
-    Escalate the issue to the human support as the issue could not resolved by the AI agent/assistant.
-    If the input is too long, make a summary as the input instead.
-    """
+        prompt_template= PromptTemplate(input_variables=["chat_history"],
+                                        template=prompts.PROMPT_TEMPLATE__DIAGNOSIS_SOLUTION)
+        prompt = prompt_template.format(chat_history=support_agent.chat_memory.to_string())
+        response = support_agent.granite_llm.invoke(prompt)
 
-    input = input.removesuffix("\nObservation")
+        # print("\033[90m(Debug: The diagnosis_and_solution tool was invoked)\033[0m")
 
-    email_body=("Hello the support team,\n\n"
-        "Please look into the issue as shown below.\n\n" 
-        f"{input}\n\n"
-        f"Below is the conversation with the user so far.\n\n{support_agent.get_chat_memory().to_string()}\n\n"
-        "Thank you & Regards,\n\nSent by AI Agent" 
-        )
+        return response
 
-    print(f"\033[90m(Debug: The escalate_to_human_support tool was invoked. An email is being sent to the support team:\n\n{email_body})\033[0m")
+    @staticmethod
+    @tool("escalate_to_human_support", return_direct=True)
+    def escalate_to_human_support(input: str):
+        """
+        Escalate the issue to the human support as the issue could not resolved by the AI agent/assistant.
+        If the input is too long, make a summary as the input instead.
+        """
 
-    result = ("Sorry I could not help resolve the issue. I've just escalated the case to the human support team, "
-              "and they will reach out to you soon.\nIf anything else, please open a new support session by type 'new session' "
-              "- or type 'quit' to exit the program. Thank you!")
-    return result
+        global support_agent
 
+        input = input.removesuffix("\nObservation")
+
+        email_body=("Hello the support team,\n\n"
+            "Please look into the issue as shown below.\n\n" 
+            f"{input}\n\n"
+            f"Below is the conversation with the user so far.\n\n{support_agent.chat_memory.to_string()}\n\n"
+            "Thank you & Regards,\n\nSent by AI Agent" 
+            )
+
+        print(f"\033[90m(Debug: The escalate_to_human_support tool was invoked. An email is being sent to the support team:\n\n{email_body})\033[0m")
+
+        result = ("Sorry I could not help resolve the issue. I've just escalated the case to the human support team, "
+                "and they will reach out to you soon.\nIf anything else, please open a new support session by type 'new session' "
+                "- or type 'quit' to exit the program. Thank you!")
+        return result
 
 
 if __name__ == "__main__":
 
+    global support_agent
+    support_agent = TechSupportAgent()
 
     is_requested_to_stop = False
-
-
-
-    # Tools
-    tools = [default_action, 
-             generate_a_clarifying_question, 
-             diagnosis_and_solution, 
-             escalate_to_human_support]
-
-    ### Set up ReAct framework ###
-
-    prompt_template_react = PromptTemplate.from_template(prompts.PROMPT_TEMPLATE__REACT)
-
-    # Construct the ReAct agent. The LLM is used as a reasoning engine
-    agent = create_react_agent(llama_llm, tools, prompt_template_react)
-
-    # Create an agent executor by passing in the agent and tools
-    agent_executor = AgentExecutor(agent=agent, 
-                                tools=tools,  
-                                verbose=AGENT_EXECUTION_VERBOSE, 
-                                handle_parsing_errors=True, 
-                                max_iterations = MAX_ITERATIONS,
-                                return_intermediate_steps=True,
-                                )
 
     ### The main conversation ### 
     print("\nStart the support session! (Type 'quit' to finish, 'new session' for a new support session)\n")
 
     # Greeting
-    greeting =  "Hello! This is the techical support. How can I help you?"
+    greeting = support_agent.greet_user("Howie")
     agent_streaming_print(greeting)
-    chat_memory.append({"Agent": greeting})
 
     user_style = Style.from_dict({
         'prompt': "#5dade2",
@@ -285,51 +267,12 @@ if __name__ == "__main__":
             if user_input.lower() == 'quit':
                 break
             elif user_input.lower() == 'new session':
-                if chat_memory:
-                    chat_memory.clear()
+                support_agent.clear_memory()
                 print("\nStart the new support session!\n")
                 continue
 
-            chat_memory.append({"User": user_input})
-
             print(f"\nAgent: \033[90mPlease wait\033[0m", end=' ', flush=True)
-            for i in range(NUMBER_OF_RETRIES + 1):
-                print(f"\033[90m...\033[0m", end=' ', flush=True)
-
-                agent_response = None
-                
-                result = {}
-                try:
-                    result = agent_executor.invoke({"user_input": user_input, "chat_history": chat_memory})
-                    agent_response = result.get('output')
-                except Exception as e:
-                    result['error'] = f"{e}"
-                    traceback.print_exc() 
-
-                if agent_response is not None and "Agent stopped due to iteration limit" in agent_response:
-                    del result['output']
-                    agent_response = None
-                    result['error'] = "Agent stopped due to iteration limit or time limit"
-
-                # if no good result returned from the model, let it retry
-                if result.get('error'):
-                    if i >= NUMBER_OF_RETRIES:
-                        intermediate_steps = result.get('intermediate_steps')
-                        if intermediate_steps:
-                            print(f"\033[90m...\033[0m", end=' ', flush=True)
-                            last_step_response = intermediate_steps[-1][-1] 
-                            agent_response = last_step_response 
-                            break
-                else:
-                    break # it's very good if it can reach here, so no need for a retry
-            # End of the Retry loop    
-
-            # after all retries, if still no result:
-            if agent_response:
-                chat_memory.append({"Agent": agent_response})
-            else:
-                agent_response = f"Sorry, something was wrong due to an error ({result.get('error')}). Please try entering the input again..."
-
+            agent_response = support_agent.query(user_input)
             print(" " * 100, end='\r')
             agent_streaming_print(agent_response)
 
